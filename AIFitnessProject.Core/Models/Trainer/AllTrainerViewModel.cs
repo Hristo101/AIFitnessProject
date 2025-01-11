ac7e00bc6f281142b36edafb200a2229c883e8e2
Hristo101 <92890172+Hristo101@users.noreply.github.com>
2025-01-11 22:02:38 +0200

    CreatedAllTrainerViewModelForAllMethod

---
+++ AIFitnessProject.Core/Models/Trainer/AllTrainerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIFitnessProject.Core.Models.Trainer
+{
+    public class AllTrainerViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ImageUrl { get; set; }
+        public string Specialization {  get; set; }
+        public int Experience { get; set; }
+    }
+}
