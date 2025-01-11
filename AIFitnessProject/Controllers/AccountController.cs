b74ca0d147cefd0cb83da44fa589f4957b604434
Hristo101 <92890172+Hristo101@users.noreply.github.com>
2025-01-11 21:57:23 +0200

    AccountControllerWorkInProgress

--- AIFitnessProject/Controllers/AccountController.cs
+++ AIFitnessProject/Controllers/AccountController.cs
@@ -1,25 +1,108 @@
 ﻿using AIFitnessProject.Core.Models.Account;
+using AIFitnessProject.Infrastructure.Common;
+using AIFitnessProject.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AIFitnessProject.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly IRepository repository;
+        public AccountController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager, IRepository _repository)
+        {
+            this.userManager = _userManager;
+            this.signInManager = _signInManager;
+            this.repository = _repository;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            RegisterViewModel model = new RegisterViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser()
+            {
+                Email = model.Email,
+                UserName = model.UserName,
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, isPersistent: false);
+
+                return RedirectToAction("All", "Announcement");
+            }
+
+            return View(model);
+        }
+
         [HttpGet]
         public async Task<IActionResult> Login()
         {
             var model = new LoginViewModel();
             return View(model);
         }
-        [HttpGet]
-        public async Task<IActionResult> Register()
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
-            var model = new RegisterViewModel();
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await repository.All<ApplicationUser>().FirstOrDefaultAsync(a => a.Email == model.Email);
+
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    if (await userManager.IsInRoleAsync(user, "Administrator"))
+                    {
+                        return RedirectToAction("AllSales", "AnnouncementAdmin", new { area = "Admin" });
+                    }
+
+                    return RedirectToAction("All", "Announcement");
+                }
+                else
+                {
+                    ModelState.AddModelError(nameof(model.Email), "Невалидни данни за вход");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(nameof(model.Email), "Невалидни данни за вход");
+            }
+
             return View(model);
+        }
+
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
