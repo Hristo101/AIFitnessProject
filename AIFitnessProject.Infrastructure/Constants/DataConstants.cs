7fe717a050939e00ad759dc523143fa139b44b37
Stanislav Ivanov <135168930+StanislavIvanov7@users.noreply.github.com>
2025-01-11 19:15:11 +0200

    CreateMealConstants

--- AIFitnessProject.Infrastructure/Constants/DataConstants.cs
+++ AIFitnessProject.Infrastructure/Constants/DataConstants.cs
@@ -10,6 +10,20 @@ namespace AIFitnessProject.Infrastructure.Constants
     {
         public static class Meal
         {
+            public const int MaxNameLength = 500;
+            public const int MinNameLength = 3;
+
+            public const int MaxRecipeLength = 2000;
+            public const int MinRecipeLength = 3;
+
+            public const int MaxImageUrlLength = 2048;
+            public const int MinImageUrlLength = 10;
+
+            public const int MaxVideoUrlLength = 2048;
+            public const int MinVideoUrlLength = 10;
+
+            public const int MaxCaloriesLength = 3000;
+            public const int MinCaloriesLength = 3;
 
         }
         public static class Diet
