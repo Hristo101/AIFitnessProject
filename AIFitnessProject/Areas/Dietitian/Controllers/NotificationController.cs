7ad11aeb741ad8e3bb5f33c1862aff04e00795ec
Stanislav Ivanov <135168930+StanislavIvanov7@users.noreply.github.com>
2025-03-30 13:58:45 +0300

    implemented notification logic for calendar between user and dietitian

--- AIFitnessProject/Areas/Dietitian/Controllers/NotificationController.cs
+++ AIFitnessProject/Areas/Dietitian/Controllers/NotificationController.cs
@@ -21,6 +21,60 @@ namespace AIFitnessProject.Areas.Dietitian.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> View(string userId, int notificationId)
+        {
+            await notificationService.MarkNotificationRead(notificationId);
+
+            var notification = await notificationService.GetNotificationById(notificationId);
+
+            switch (notification.Source)
+            {
+                case "Calendar":
+                    return RedirectToAction("UserCalendar", "Calendar", new { id = userId });
+                case "RequestsToCoaches":
+                    return RedirectToAction("All", "MyRequests");
+                case "TrainingPlanDetails":
+                    return RedirectToAction("AllUsersWorkouts", "Workout", new { id = userId });
+                case "Comments":
+                    return RedirectToAction("DashBoard", "Account");
+                default:
+                    return RedirectToAction("AllRejectedTrainingPlan", "TrainingPlan");
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> MarkAllAsRead()
+        {
+            try
+            {
+                await notificationService.MarkAllAsRead(GetUserId());
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> ChangeNotificationStatus(int id)
+        {
+            await notificationService.MarkNotificationRead(id);
+            return Json(new { success = true });
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await notificationService.DeleteNotification(id);
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+
+        }
         private string GetUserId()
         {
             return User.FindFirstValue(ClaimTypes.NameIdentifier);
